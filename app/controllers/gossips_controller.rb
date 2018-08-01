ace2ff9d30bfe503493fd70ba1e6168aef6c817d
christopheleray <christophe.leray1@gmail.com>
2018-08-01 19:03:03 +0200

    ajout de l'option delete

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -41,6 +41,12 @@ class GossipsController < ApplicationController
     redirect_to gossip_path(gossip.id)
   end 
 
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossips_path
+  end
+
   private
 
   def gossip_params
