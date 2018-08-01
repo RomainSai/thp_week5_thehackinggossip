d5b93b73e4e3f69083dda760ea49542e6561e8b8
christopheleray <christophe.leray1@gmail.com>
2018-08-01 17:05:49 +0200

    navbar bootstrap et forme

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,18 @@
 class StaticPagesController < ApplicationController
   def home
   end
+
+  def index
+    @gossip = Gossip.all
+  end
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def show
+    @gossip = Gossip.find(params[:id])
+  end
+
+
 end
