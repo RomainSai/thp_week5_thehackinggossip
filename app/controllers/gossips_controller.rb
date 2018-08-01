20290b25e5e5ce12985c39700ac8ff6efa14e424
christopheleray <christophe.leray1@gmail.com>
2018-08-01 18:47:48 +0200

    ajout update et edit

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -2,10 +2,11 @@ class GossipsController < ApplicationController
 
   def index
     @gossip = Gossip.all
-    puts 'j affiche depuis new'
+    puts 'j affiche depuis index'
   end
 
   def new
+    puts "j'affiche depuis new"
     @gossip = Gossip.new
     puts 'j affiche depuis new'
     puts params.inspect
@@ -19,15 +20,26 @@ class GossipsController < ApplicationController
     #  title: params[:gossip][:title],
     #   content: params[:gossip][:content],
     #   anonymous_gossiper: params[:gossip][:anonymous_gossiper])
-    puts 'j affiche depuis new'
+    puts 'j affiche depuis create'
     #redirect_to action: 'show', id: @gossip.title
     redirect_to gossip_path(gossip.id)
   end
 
 
   def show
+    puts "j'affiche depuis show"
     @gossip = Gossip.find(params[:id])
   end
+
+  def edit
+    puts "j'affiche depuis edit"
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    gossip = Gossip.create(gossip_params)
+    redirect_to gossip_path(gossip.id)
+  end 
 
   private
 
